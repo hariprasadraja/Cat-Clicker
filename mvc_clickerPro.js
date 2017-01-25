9137331948eb3534488502d7f6e9023ed1dd4a62
hariprasad <Hariprasad@benseron.com>
2017-01-25 12:47:54 +0530

    Updated LocalStorage

--- mvc_clickerPro.js
+++ mvc_clickerPro.js
@@ -18,82 +18,84 @@
 //     under certain conditions; type `show c' for details.
 
 console.log("MVC_clickerPro.js");
-var model ={
+
+const GET = "Set Data form localStorage"
+const SET = "Get Data form localStorage"
+//  Model
+var model = {
   cats: [{
     id: 0,
-    name:"cat1",
+    name: "cat1",
     clicks: 0,
-    ImageURL:"cat1.jpg"
+    ImageURL: "cat1.jpg"
   }, {
     id: 1,
-    name:"cat2",
+    name: "cat2",
     clicks: 0,
     ImageURL: "cat2.jpg"
 
   }, {
     id: 2,
-    name:"cat3",
+    name: "cat3",
     clicks: 0,
     ImageURL: "cat3.jpg"
 
   }, {
     id: 3,
-    name:"cat4",
+    name: "cat4",
     clicks: 0,
     ImageURL: "cat4.jpg"
 
   }, {
     id: 4,
-    name:"cat5",
-    //  clicks: null
+    name: "cat5",
+
     clicks: 0,
     ImageURL: "cat5.jpg"
 
   }],
-  insertCats:function(catname,caturl,catclick){
 
-      let index =this.cats.length;
-            for(i=0;i<index;i++){
-              if(Octopus.getCatName(i) === catname){
-              prompt("cat name is already present");
-           return ""
-        }
-         if(Octopus.getCatImageURL(i) === catclick){
-            prompt("cat image is aleray present");
-            return ""
-        }
-       }
-       let newcat = {
-           id: index,
-           name:catname,
-           //  clicks: null
-           clicks: catclick,
-           ImageURL:caturl
-       }
-       this.cats[index]=newcat;
-       console.log(model.cats);
+  insertCats: function(catname, caturl, catclick) {
 
-       return index
+    let index = this.cats.length;
+    for (i = 0; i < index; i++) {
+      if (Octopus.getCatName(i) === catname) {
+        alert("cat name is already present");
+        return ""
+      }
+      if (Octopus.getCatImageURL(i) === catclick) {
+        alert("cat image is aleray present");
+        return ""
+      }
+    }
+    let newcat = {
+      id: index,
+      name: catname,
+      clicks: catclick,
+      ImageURL: caturl
+    }
+    this.cats.push(newcat);
+    Octopus.storageLocal(SET);
+  }
 }
-};
 
+// Contoller
 var Octopus = {
-  init:function(){
+  init: function() {
     Adminform = document.forms["Adminform"];
-     Adminform.style.display= "none";
-     // for Admin button
-     document.getElementById('Admin').addEventListener('click',function(){
-       Adminform.style.display= "block";
-     });
-     //for Logout button
-     document.getElementById('logout').addEventListener('click',function(){
-       Adminform.style.display="none";
-     });
-     view.init();
+    Adminform.style.display = "none";
+    // for Admin button
+    document.getElementById('Admin').addEventListener('click', function() {
+      Adminform.style.display = "block";
+    });
+    //for Logout button
+    document.getElementById('logout').addEventListener('click', function() {
+      Adminform.style.display = "none";
+    });
+    view.init();
   },
 
   getCatName: function(i) {
-    console.log(i);
     this.catname = model.cats[i].name;
     if (this.catname != "") {
       return this.catname
@@ -101,19 +103,19 @@ var Octopus = {
       return "";
     }
   },
-  getCatImageURL: function(i) {
 
+  getCatImageURL: function(i) {
     this.ImageURL = model.cats[i].ImageURL;
     if (this.ImageURL != "") {
       return this.ImageURL
     } else {
-        return "";
+      return "";
     }
   },
+
   getCatClicks: function(i) {
     this.catclick = model.cats[i].clicks;
     if (this.catclick !== "") {
-
       return this.catclick;
     } else {
       return ""
@@ -143,65 +145,72 @@ var Octopus = {
       }
     }
   },
-  submit:function(){
-      let catname=Adminform['catname'].value
-      let ImageURL=Adminform['ImageURL'].value
-      let Clicks=Adminform['Clicks'].value
-      model.insertCats(catname,ImageURL,Clicks);
-      view.init(true);
-      event.preventDefault();
 
+  submit: function() {
+    let catname = Adminform['catname'].value
+    let ImageURL = Adminform['ImageURL'].value
+    let Clicks = Adminform['Clicks'].value
+    model.insertCats(catname, ImageURL, Clicks);
+    view.init(true);
+  },
 
+  getCats: function() {
+    return model.cats;
+  },
 
+  storageLocal: function(flag) {
+    if(flag===GET){
+    let x = JSON.stringify(this.getCats());
+
+    if (localStorage.getItem('cats') === null) {
+      localStorage.setItem('cats', x);
+    } else {
+      x = JSON.parse(localStorage.getItem('cats'));
+      model.cats = x;
+    }
+  }
+  else if(flag===SET){
+    let x = JSON.stringify(this.getCats());
+    localStorage.setItem("cats", x);
   }
 }
+}
+// View
 var view = {
+
   init: function(flag) {
     this.catImageRender = document.getElementById('catview');
     this.catDetailsRender = document.getElementById('catdetails');
     let buttonToRender = document.getElementById('buttontemplate')
     let buttonTemplate = new Array();
-     this.catImageRender.addEventListener('click', Octopus.incrementCatClick);
-//    console.log(model.cats);
-  //  console.log(model.cats.length);
-    if(flag==true){
-     buttonToRender.innerHTML = "";
+    this.catImageRender.addEventListener('click', Octopus.incrementCatClick);
+    if (flag == true) {
+      buttonToRender.innerHTML = "";
     }
-    for (i = 0; i< model.cats.length; i++) {
+    Octopus.storageLocal(GET);  // Get localStorage data and save it to model.cats or create a new localStorage data and save it to model.cats
+    for (i = 0; i < model.cats.length; i++) {
       buttonTemplate[i] = '<li> <a onclick = "view.render(' + Octopus.getCatId(i) + ')" > ' + Octopus.getCatName(i) + '</a> </li> ';
       buttonToRender.innerHTML = buttonToRender.innerHTML + buttonTemplate[i];
     }
   },
-  getTemplate: function(i,renderflag) {
-    console.log("getTemplate");
-    console.log(i);
-    console.log(renderflag);
-    if(renderflag !== "" && i !== "") {
-    if(renderflag==="catImage") {
-      this.template ="<img id='" + Octopus.getCatName(i) + "' src='" + Octopus.getCatImageURL(i)+"'>";
-    }
-    else if(renderflag==="catDetails"){
-      this.template="<h5><span id='cat-name'>Cat Name:" + Octopus.getCatName(i) + " </span>  <p> Click Me..</p> </h5><h4> No of Clicks :" + Octopus.getCatClicks(i) + "</h4>";
-    }
-    return this.template;
-  }
-  else{
-     return prompt("Your cat is not found");
-  }
-},
 
-  //   this.template = "<img id='" + Octopus.getCatName(i) + "' src='" + Octopus.getCatImageURL(i) + "'><h5><span id='cat-name'>Cat Name:" + Octopus.getCatName(i) + " </span>  <p> Click Me..</p> </h5><h4> No of Clicks :" + Octopus.getCatClicks(i) + "</h4>";
-  //   return this.template;
-//},
+  getTemplate: function(i, renderflag) {
+    if (renderflag !== "" && i !== "") {
+      if (renderflag === "catImage") {
+        this.template = "<img id='" + Octopus.getCatName(i) + "' src='" + Octopus.getCatImageURL(i) + "'>";
+      } else if (renderflag === "catDetails") {
+        this.template = "<h5><span id='cat-name'>Cat Name:" + Octopus.getCatName(i) + " </span>  <p> Click Me..</p> </h5><h4> No of Clicks :" + Octopus.getCatClicks(i) + "</h4>";
+      }
+      return this.template;
+    } else {
+      return prompt("Your cat is not found");
+    }
+  },
+
   render: function(id) {
-    // let elementtorender = document.getElementById('catview');
-    // elementtorender.innerHTML = view.getTemplate(id);
-    this.catImageRender.innerHTML=this.getTemplate(id,"catImage");
-    this.catDetailsRender.innerHTML=this.getTemplate(id,"catDetails");
-
+    this.catImageRender.innerHTML = this.getTemplate(id, "catImage");
+    this.catDetailsRender.innerHTML = this.getTemplate(id, "catDetails");
   }
-
-
 }
 
 Octopus.init();
