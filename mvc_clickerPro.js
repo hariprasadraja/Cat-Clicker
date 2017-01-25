1aff4a275deb1cd03744a9fbec886edbfe19357f
hariprasad <Hariprasad@benseron.com>
2017-01-25 12:55:07 +0530

    updated constant

--- mvc_clickerPro.js
+++ mvc_clickerPro.js
@@ -21,7 +21,9 @@ console.log("MVC_clickerPro.js");
 
 const GET = "Set Data form localStorage"
 const SET = "Get Data form localStorage"
-//  Model
+const CIMAGE = "CatImage"
+const CDETAILS = "CatDetails"
+  //  Model
 var model = {
   cats: [{
     id: 0,
@@ -81,101 +83,100 @@ var model = {
 
 // Contoller
 var Octopus = {
-  init: function() {
-    Adminform = document.forms["Adminform"];
-    Adminform.style.display = "none";
-    // for Admin button
-    document.getElementById('Admin').addEventListener('click', function() {
-      Adminform.style.display = "block";
-    });
-    //for Logout button
-    document.getElementById('logout').addEventListener('click', function() {
+    init: function() {
+      Adminform = document.forms["Adminform"];
       Adminform.style.display = "none";
-    });
-    view.init();
-  },
+      // for Admin button
+      document.getElementById('Admin').addEventListener('click', function() {
+        Adminform.style.display = "block";
+      });
+      //for Logout button
+      document.getElementById('logout').addEventListener('click', function() {
+        Adminform.style.display = "none";
+      });
+      view.init();
+    },
 
-  getCatName: function(i) {
-    this.catname = model.cats[i].name;
-    if (this.catname != "") {
-      return this.catname
-    } else {
-      return "";
-    }
-  },
+    getCatName: function(i) {
+      this.catname = model.cats[i].name;
+      if (this.catname != "") {
+        return this.catname
+      } else {
+        return "";
+      }
+    },
 
-  getCatImageURL: function(i) {
-    this.ImageURL = model.cats[i].ImageURL;
-    if (this.ImageURL != "") {
-      return this.ImageURL
-    } else {
-      return "";
-    }
-  },
+    getCatImageURL: function(i) {
+      this.ImageURL = model.cats[i].ImageURL;
+      if (this.ImageURL != "") {
+        return this.ImageURL
+      } else {
+        return "";
+      }
+    },
 
-  getCatClicks: function(i) {
-    this.catclick = model.cats[i].clicks;
-    if (this.catclick !== "") {
-      return this.catclick;
-    } else {
-      return ""
+    getCatClicks: function(i) {
+      this.catclick = model.cats[i].clicks;
+      if (this.catclick !== "") {
+        return this.catclick;
+      } else {
+        return ""
 
-    }
-  },
+      }
+    },
 
-  getCatId: function(i) {
-    this.catclick = model.cats[i].id;
-    if (this.catclick !== "") {
+    getCatId: function(i) {
+      this.catclick = model.cats[i].id;
+      if (this.catclick !== "") {
 
-      return this.catclick;
-    } else {
+        return this.catclick;
+      } else {
 
-      return ""
-    }
-  },
+        return ""
+      }
+    },
 
-  incrementCatClick: function() {
-    let firstChildId = this.childNodes[0].id;
-    for (i = 0; i < model.cats.length; i++) {
-      if (model.cats[i].name === firstChildId) {
-        ++model.cats[i].clicks;
+    incrementCatClick: function() {
+      let firstChildId = this.childNodes[0].id;
+      for (i = 0; i < model.cats.length; i++) {
+        if (model.cats[i].name === firstChildId) {
+          ++model.cats[i].clicks;
 
-        view.render(i);
-        break;
+          view.render(i);
+          break;
+        }
+      }
+    },
+
+    submit: function() {
+      let catname = Adminform['catname'].value
+      let ImageURL = Adminform['ImageURL'].value
+      let Clicks = Adminform['Clicks'].value
+      model.insertCats(catname, ImageURL, Clicks);
+      view.init(true);
+    },
+
+    getCats: function() {
+      return model.cats;
+    },
+
+    storageLocal: function(flag) {
+      if (flag === GET) {
+        let x = JSON.stringify(this.getCats());
+
+        if (localStorage.getItem('cats') === null) {
+          localStorage.setItem('cats', x);
+        } else {
+          x = JSON.parse(localStorage.getItem('cats'));
+          model.cats = x;
+        }
+      } else if (flag === SET) {
+        let x = JSON.stringify(this.getCats());
+        localStorage.setItem("cats", x);
       }
     }
-  },
-
-  submit: function() {
-    let catname = Adminform['catname'].value
-    let ImageURL = Adminform['ImageURL'].value
-    let Clicks = Adminform['Clicks'].value
-    model.insertCats(catname, ImageURL, Clicks);
-    view.init(true);
-  },
-
-  getCats: function() {
-    return model.cats;
-  },
-
-  storageLocal: function(flag) {
-    if(flag===GET){
-    let x = JSON.stringify(this.getCats());
-
-    if (localStorage.getItem('cats') === null) {
-      localStorage.setItem('cats', x);
-    } else {
-      x = JSON.parse(localStorage.getItem('cats'));
-      model.cats = x;
-    }
   }
-  else if(flag===SET){
-    let x = JSON.stringify(this.getCats());
-    localStorage.setItem("cats", x);
-  }
-}
-}
-// View
+  // View
 var view = {
 
   init: function(flag) {
@@ -187,7 +188,7 @@ var view = {
     if (flag == true) {
       buttonToRender.innerHTML = "";
     }
-    Octopus.storageLocal(GET);  // Get localStorage data and save it to model.cats or create a new localStorage data and save it to model.cats
+    Octopus.storageLocal(GET); // Get localStorage data and save it to model.cats or create a new localStorage data and save it to model.cats
     for (i = 0; i < model.cats.length; i++) {
       buttonTemplate[i] = '<li> <a onclick = "view.render(' + Octopus.getCatId(i) + ')" > ' + Octopus.getCatName(i) + '</a> </li> ';
       buttonToRender.innerHTML = buttonToRender.innerHTML + buttonTemplate[i];
@@ -196,9 +197,9 @@ var view = {
 
   getTemplate: function(i, renderflag) {
     if (renderflag !== "" && i !== "") {
-      if (renderflag === "catImage") {
+      if (renderflag === CIMAGE) {
         this.template = "<img id='" + Octopus.getCatName(i) + "' src='" + Octopus.getCatImageURL(i) + "'>";
-      } else if (renderflag === "catDetails") {
+      } else if (renderflag === CDETAILS) {
         this.template = "<h5><span id='cat-name'>Cat Name:" + Octopus.getCatName(i) + " </span>  <p> Click Me..</p> </h5><h4> No of Clicks :" + Octopus.getCatClicks(i) + "</h4>";
       }
       return this.template;
@@ -208,8 +209,8 @@ var view = {
   },
 
   render: function(id) {
-    this.catImageRender.innerHTML = this.getTemplate(id, "catImage");
-    this.catDetailsRender.innerHTML = this.getTemplate(id, "catDetails");
+    this.catImageRender.innerHTML = this.getTemplate(id,CIMAGE);
+    this.catDetailsRender.innerHTML = this.getTemplate(id, CDETAILS);
   }
 }
 
